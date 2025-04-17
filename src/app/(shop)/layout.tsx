b6d5125002c27b34decd3275850fff05516c0eed
Manuel Salvadores <patriciozhogan@gmail.com>
2025-04-17 14:04:58 -0300

    a ver

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -8,10 +8,10 @@ export default function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <main className='min-h-screen flex'>
+    <main className='min-h-screen'>
       <TopMenu />
       <Sidebar />
-      <div className='flex-1 px-0 sm:px-10'>{children}</div>
+      <div className='px-0 sm:px-10'>{children}</div>
       <Footer />
     </main>
   );
